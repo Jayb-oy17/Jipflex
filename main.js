1f37da896f6fb40b207aad96908f899a1eac038c
Jipson <ajiboladiekola16@gmail.com>
2025-06-25 09:22:57 +0100

    final commit

--- main.js
+++ main.js
@@ -38,6 +38,29 @@ navs.forEach((button) => {
   });
 });
 
+// ACTIVE SCROLL
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".navLInk");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop - 100;
+    const sectionHeight = section.clientHeight;
+    if (pageYOffset >= sectionTop && pageYOffset < sectionTop + sectionHeight) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach((link) => {
+    link.classList.remove("active");
+    if (link.getAttribute("href") === `#${current}`) {
+      link.classList.add("active");
+    }
+  });
+});
+
 // PORTFOLIO TAB COMPONENT
 const tabButtons = document.querySelectorAll(".ptTab");
 const tabContents = document.querySelectorAll(".pt");
